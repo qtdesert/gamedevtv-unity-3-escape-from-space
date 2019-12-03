e6ade5b06798c50656728b80e225c30ea56e7ade
Quentin Désert <qtdesert@gmail.com>
2019-12-02 20:26:38 -0600

    Basic input binding, adding physics and rigidbodies

--- Assets/Spaceship.cs
+++ Assets/Spaceship.cs
@@ -1,18 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spaceship : MonoBehaviour
-{
+public class Spaceship : MonoBehaviour {
+    
+    enum Direction { Left, Right };
+    Direction rotation;
+    Rigidbody rigidBody;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.mass = 0.1f;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        ProcessInput();
+    }
+
+    private void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space)) {
+            Thrust();
+        }
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) {
+            if (rotation == Direction.Left) {
+                RotateLeft();
+            } else {
+                RotateRight();
+            }
+        }
+        else if (Input.GetKey(KeyCode.A)) {
+            RotateLeft();
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            RotateRight();
+        }
+    }
+
+    private void Thrust() {
+        rigidBody.AddRelativeForce(Vector3.up);
+    }
+
+    private void RotateLeft() {
+        rotation = Direction.Left;
+        print("Rotating left");
+    }
+
+    private void RotateRight() {
+        rotation = Direction.Right;
+        print("Rotating right");
     }
 }
